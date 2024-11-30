718bbc21b715f938455fc230a29bad6cf4f90c37
Shahmeer-Khan99 <shahmeerskhan@gmail.com>
2024-11-30 22:23:32 +0100

    Matrix Build

---
+++ 4-matrix-computation/project/CMakeFiles/gpu_gemm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gpu_gemm.dir/cmake_device_link.o"
+  "CMakeFiles/gpu_gemm.dir/src/gpu_GEMM.cu.o"
+  "CMakeFiles/gpu_gemm.dir/src/gpu_GEMM.cu.o.d"
+  "gpu_gemm"
+  "gpu_gemm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/gpu_gemm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
